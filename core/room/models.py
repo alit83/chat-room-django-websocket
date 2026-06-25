82e7b3ff0d8f066731638b4d41d26e2fa1143e75
alit83 <alitoosi1233@gmail.com>
2026-06-25 11:56:07 +0330

    feat(roo): create room model

---
+++ core/room/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+class ModelType(models.IntegerChoices):
+    pv = 1, _("pv")
+    group_public = 2, _("group_public")
+    group_private = 3, _("group_private")
+
+
+class Room(models.Model):
+    user = models.ManyToManyField('accounts.Profile')
+    creator=models.ForeignKey('accounts.Profile',on_delete=models.CASCADE, related_name='creator_profile')
+    model = models.IntegerField(choices=ModelType.choices)
+    name = models.CharField(max_length=255)
+    link = models.SlugField(unique=True,null=True)
+    profile=models.ImageField(upload_to='room_profile/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
