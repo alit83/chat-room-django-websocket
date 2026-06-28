aebd407db88a26393373f181b4c55836ad01bad2
alit83 <alitoosi1233@gmail.com>
2026-06-28 19:37:35 +0330

    feat(accounts): set token refresh view and token verify view and tested with postman

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import (
+    TokenVerifyView,TokenRefreshView
+)
 
 urlpatterns=[
     path('registration/',views.SignUpApiView.as_view(),name='registration'),
-    path('login/',views.LoginApiView.as_view(),name='login')
+    path('login/',views.LoginApiView.as_view(),name='login'),
+    path('jwt/refresh-token/',TokenRefreshView.as_view(),name='refresh_token'),
+    path('jwt/token-verification/',TokenVerifyView.as_view(),name='token_verification'),
 ]
